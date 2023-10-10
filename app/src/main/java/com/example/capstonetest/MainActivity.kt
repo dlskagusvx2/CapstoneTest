62a492e2169aa27983c2827d8c0a1ea744c3c134
dlskagusvx2 <dlskagusvxx3@naver.com>
2023-10-10 21:00:38 +0900

    2023-10-10 gpt와 연결은 됐음 스크립트 불러오는게 안됨 아마 대기시간 문제인듯

--- app/src/main/java/com/example/capstonetest/MainActivity.kt
+++ app/src/main/java/com/example/capstonetest/MainActivity.kt
@@ -10,6 +10,7 @@ import android.webkit.WebViewClient
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.capstonetest.databinding.ActivityMainBinding
+import com.google.gson.JsonArray
 import com.google.gson.JsonObject
 import com.google.gson.JsonParser
 import okhttp3.MediaType
@@ -18,6 +19,7 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.RequestBody.Companion.toRequestBody
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
     private val binding by lazy {
@@ -25,8 +27,10 @@ class MainActivity : AppCompatActivity() {
     }
 
     private lateinit var webView: WebView
-    val apiKey = "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"
-    val endpoint = "https://api.openai.com/v1/engines/gpt-3.5-turbo/completions"
+    val apiKey = ""// api 키 입력해야함
+    val endpoint = "https://api.openai.com/v1/chat/completions"
+    val model = "gpt-3.5-turbo" // 사용할 모델 (GPT-3 Turbo)
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -40,9 +44,9 @@ class MainActivity : AppCompatActivity() {
         webSettings.userAgentString = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3" // User Agent 설정
 
         webView.webChromeClient = WebChromeClient()
-        askToChatGPT()
 
-        /*webView.webViewClient = object : WebViewClient() {
+
+        webView.webViewClient = object : WebViewClient() {
             override fun onPageFinished(view: WebView?, url: String?) {
                 // 페이지 로딩이 완료되면 5초 기다린 후 JavaScript 코드를 실행
                 Handler(Looper.getMainLooper()).postDelayed({
@@ -86,42 +90,54 @@ class MainActivity : AppCompatActivity() {
         webView.loadUrl("https://www.youtube.com/watch?v=fkS-nARvbbs")
 
         // WebView에서 JavaScript 코드 실행 결과를 처리하는 인터페이스
-        webView.addJavascriptInterface(this, "android")*/
+        webView.addJavascriptInterface(this, "android")
     }
 
-    fun askToChatGPT(){
-        val inputText = "안녕"
+    private fun askToChatGPT(q: String) {
+        val messagesArray = JsonArray()
+        val userMessage = JsonObject()
+        userMessage.addProperty("role", "user")
+        userMessage.addProperty("content", "${q} 유튜브 영상 스크립트인데 요약해줘.") // 사용자 메시지를 추가
+        messagesArray.add(userMessage)
+
         val client = OkHttpClient()
         val jsonMediaType = "application/json; charset=utf-8".toMediaType()
-        val requestBody = """
-        {
-            "prompt": "$inputText",
-            "max_tokens": 50
-        }
-    """.trimIndent().toRequestBody(jsonMediaType)
+        val requestBody = JsonObject()
+        requestBody.add("messages", messagesArray)
+        requestBody.addProperty("model", model) // 모델 명시
 
         val request = Request.Builder()
             .url(endpoint)
             .addHeader("Authorization", "Bearer $apiKey")
-            .post(requestBody)
+            .post(requestBody.toString().toRequestBody(jsonMediaType))
             .build()
 
         Thread {
-            val response = client.newCall(request).execute()
-            val responseBody = response.body?.string()
+            try {
+                val response = client.newCall(request).execute()
+                val responseBody = response.body?.string()
+                val jsonResponse = JsonParser.parseString(responseBody) as JsonObject
+                val choicesArray = jsonResponse.getAsJsonArray("choices")
 
-            if (responseBody != null) {
-                val jsonResponse = JsonParser.parseString(responseBody) as JsonObject // JSON 파싱
-                val completion = jsonResponse.getAsJsonArray("choices")[0].asJsonObject.getAsJsonPrimitive("text").asString
+                if (choicesArray != null && choicesArray.size() > 0) {
+                    val assistantMessage = choicesArray[0].asJsonObject.getAsJsonObject("message")
+                    val content = assistantMessage.getAsJsonPrimitive("content").asString
 
-                runOnUiThread {
-                    // UI 업데이트를 메인 스레드에서 수행
-                    binding.textbox.text = completion
+                    runOnUiThread {
+                        // UI 업데이트를 메인 스레드에서 수행
+                        binding.textbox.text = content ?: "실패"
+                    }
+                } else {
+                    runOnUiThread {
+                        // UI 업데이트를 메인 스레드에서 수행
+                        binding.textbox.text = "실패"
+                    }
                 }
-            } else {
+            } catch (e: IOException) {
+                e.printStackTrace()
                 runOnUiThread {
                     // UI 업데이트를 메인 스레드에서 수행
-                    binding.textbox.text = "실패"
+                    binding.textbox.text = "오류 발생 ${e}"
                 }
             }
         }.start()
@@ -129,11 +145,10 @@ class MainActivity : AppCompatActivity() {
     // JavaScript에서 호출할 메서드
     @JavascriptInterface
     fun onTextExtracted(result: String) {
-        runOnUiThread {
-            // 추출된 텍스트를 사용하여 필요한 작업을 수행
-            binding.textbox.text = if (result.isNullOrEmpty()) "텍스트 추출 실패" else result
-        }
+        askToChatGPT(result)
     }
+
+
 
     // JavaScript 코드 실행 메서드
     private fun runJavaScriptCode(code: String) {
