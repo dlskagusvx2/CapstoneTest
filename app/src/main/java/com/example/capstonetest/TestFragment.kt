f7196772f5ea9a6aa03bb50adbc2758cd01c1ae0
cmw <alsdn5625@naver.com>
2023-10-14 13:48:38 +0900

    layout 만드는중

---
+++ app/src/main/java/com/example/capstonetest/TestFragment.kt
@@ -0,0 +1,43 @@
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.capstonetest.R
+
+class TestFragment : Fragment() {
+    private var image: Int? = null
+    private var text: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            image = it.getInt("image", 0)
+            text = it.getString("text", "")
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_test, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        imageView.setImageResource(image!!)
+        textView.text = text
+    }
+
+    companion object {
+        fun newInstance(image: Int, text: String) =
+            TestFragment().apply {
+                arguments = Bundle().apply {
+                    putInt("image", image)
+                    putString("text", text)
+                }
+            }
+    }
+}
