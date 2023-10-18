e9996e8a04acc32700c1967f2c29201a3a0e4f5e
cmw <alsdn5625@naver.com>
2023-10-18 12:01:56 +0900

    sub layout 만드는중 현재 folding view인데 다른 걸로 바꿀지 고민중

--- app/src/main/java/com/example/capstonetest/SubActivity.kt
+++ app/src/main/java/com/example/capstonetest/SubActivity.kt
@@ -4,11 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.capstonetest.databinding.ActivitySubBinding
+import com.ramotion.foldingcell.FoldingCell
 
 class SubActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivitySubBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -17,6 +19,18 @@ class SubActivity : AppCompatActivity() {
         }
         val url = intent.getStringExtra("url")
         binding.txt.text = url.toString()
+
+        // get our folding cell
+        val fc = findViewById<FoldingCell>(R.id.folding_cell)
+
+        // attach click listener to folding cell
+        fc.setOnClickListener { v ->
+            fc.toggle(false)
     }
 
+
+    }
 }
+
+
+
